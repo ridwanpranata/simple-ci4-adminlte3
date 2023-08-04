e91e957729b7fc4c647ff71174ccf9b7642d8829
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2023-08-04 11:09:59 +0700

    Integrate to AdminLTE 3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,7 +29,13 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index'); 
+// => default controller
+$routes->get('/dashboard', 'DashboardController::index',['as' => 'dashboard']);
+
+$routes->get('/books', 'BookController::index',['as' => 'books']);
+
+
 
 /*
  * --------------------------------------------------------------------
